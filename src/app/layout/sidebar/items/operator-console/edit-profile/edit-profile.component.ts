099c5916f81d0ed8d0c2327dd88c90c6d3e92940
Rao Muhammad Huzaifa <mhuzaifarao232@gmail.com>
2026-05-17 01:43:55 +0500

    feat: enhance edit profile component with back button and preview functionality

--- src/app/layout/sidebar/items/operator-console/edit-profile/edit-profile.component.ts
+++ src/app/layout/sidebar/items/operator-console/edit-profile/edit-profile.component.ts
@@ -1,12 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { ProfileFormComponent } from '@gbxm/shared/components/profile-form/profile-form.component';
 
 @Component({
   selector: 'app-edit-profile',
   standalone: true,
-  imports: [ProfileFormComponent],
+  imports: [ProfileFormComponent, MatButtonModule, MatIconModule],
   templateUrl: './edit-profile.component.html',
   styleUrl: './edit-profile.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class EditProfileComponent {}
+export class EditProfileComponent {
+  isPreviewing = signal(false);
+}
